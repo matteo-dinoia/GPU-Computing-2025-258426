9fdde9e5bb6e8e723dbd72f3b6d307e320e71e16
Matteo Di Noia <dmatteo002@gmail.com>
2025-05-19 20:38:28 +0200

    Massive remake of main.c

--- assingnment1/cpu_version/main.c
+++ assingnment1/cpu_version/main.c
@@ -1,4 +1,3 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -9,42 +8,58 @@
 #include "include/vec_matrix.h"
 #include "include/mtx.h"
 
-#define WARM_CYCLES 100
-#define TEST_CYCLES 400
-#define INPUT_FILENAME "../datasets/mycielskian3.mtx"
+#define WARM_CYCLES 1
+#define TEST_CYCLES 20
+#define INPUT_FILENAME "../datasets/mawi_201512020330.mtx"
 
 #define OK true
 #define ERR false
 
 // ASSUME it is zeroed the res vector
-void gemm_sparse_coo(const int *cx, const int *cy, const M_TYPE *vals, const M_TYPE *vec, M_TYPE *res, const int NON_ZERO) {
+void spmv_coo(const int *cx, const int *cy, const M_TYPE *vals, const M_TYPE *vec, M_TYPE *res, const int NON_ZERO) {
     if (cx == NULL || cy == NULL || vals == NULL || vec == NULL || res == NULL) {
-        printf("NULL pointeri in GEMM sparse\n");
+        printf("NULL pointeri in spmv sparse\n");
         return;
     }
 
-    for (int i = 0; i < NON_ZERO; i++) {
-        const int row = cy[i];
-        const int col = cx[i];
-
-        res[row] += vec[col] * vals[i];
+    register int i;
+    for (i = 0; i < NON_ZERO; i++) {
+        res[cy[i]] += vec[cx[i]] * vals[i];
     }
 }
 
 // ASSUME it is zeroed the res vector
-void gemm_sparse_csr(const int *cx, const int *csr_y, const M_TYPE *vals, const M_TYPE *vec, M_TYPE *res, const int ROWS) {
+void spmv_csr(const int *cx, const int *csr_y, const M_TYPE *vals, const M_TYPE *vec, M_TYPE *res, const int ROWS) {
     if (cx == NULL || csr_y == NULL || vals == NULL || vec == NULL || res == NULL) {
-        printf("NULL pointer in GEMM sparse\n");
+        printf("NULL pointer in spmv sparse\n");
         return;
     }
 
+    register int start, end, i;
     for (int row = 0; row < ROWS; row++) {
-        const int start = csr_y[row];
-        const int end = csr_y[row + 1];
+        start = csr_y[row];
+        end = csr_y[row + 1];
 
-        for (int i = start; i < end; i++) {
-            const int col = cx[i];
-            res[row] += vec[col] * vals[i];
+        for (i = start; i < end; i++) {
+            res[row] += vec[cx[i]] * vals[i];
+        }
+    }
+}
+
+// ASSUME it is zeroed the res vector
+void optimized_spmv_csr(const int *cx, const int *csr_y, const M_TYPE *vals, const M_TYPE *vec, M_TYPE *res, const int ROWS, const int NON_ZERO) {
+    if (cx == NULL || csr_y == NULL || vals == NULL || vec == NULL || res == NULL) {
+        printf("NULL pointer in spmv sparse\n");
+        return;
+    }
+
+    register int i = 0;
+    register int end = 0;
+    for (int row = 0; row < ROWS; row++) {
+        end = csr_y[row + 1];
+
+        for (; i < end; i++) {
+            res[row] += vec[cx[i]] * vals[i];
         }
     }
 }
@@ -63,9 +78,10 @@ void convert_to_csr(const int *cy, int *csr_y, const int NON_ZERO, const int ROW
         csr_y[cy[i] + 1]++;
     }
 
-    int sum = 0;
+    int old = 0;
     for (int i = 0; i < ROWS + 1; i++) {
-        sum = csr_y[i] = csr_y[i] + sum;
+        csr_y[i] += old;
+        old = csr_y[i];
     }
 }
 
@@ -82,7 +98,7 @@ int main() {
     FILE *file;
     TIMER_DEF(0);
     TIMER_DEF(1);
-    srand(time(0) * 5);
+    srand(time(NULL));
 
     // HEADER READING --------------------------------------------------------------
     TIMER_TIME(0, {
@@ -99,13 +115,14 @@ int main() {
     // VARIABLE CREATION ----------------------------------------------------------
     int *x = calloc(NON_ZERO, sizeof(int));
     int *y = calloc(NON_ZERO, sizeof(int));
-    int *csr_y = calloc((NON_ZERO + 1), sizeof(int));
+    int *csr_y = calloc(ROWS + 1, sizeof(int));
     M_TYPE *vals = calloc(NON_ZERO, sizeof(M_TYPE));
     M_TYPE *vec = calloc(COLS, sizeof(M_TYPE));
     M_TYPE *res1 = calloc(ROWS, sizeof(M_TYPE));
     M_TYPE *res2 = calloc(ROWS, sizeof(M_TYPE));
     double *times_coo = calloc(TEST_CYCLES, sizeof(double));
     double *times_csr = calloc(TEST_CYCLES, sizeof(double));
+    double *times_opt_csr = calloc(TEST_CYCLES, sizeof(double));
 
     // DATA CREATION ---------------------------------------------------------------
     TIMER_TIME(0, {
@@ -113,8 +130,9 @@ int main() {
         if (status == ERR) {
             goto free;
         }
-
     });
+    printf("READ DATA: %fms\n", TIMER_ELAPSED(0) / 1.e3);
+
 
     gen_random_vec_double(vec, COLS);
 
@@ -124,33 +142,44 @@ int main() {
     });
     printf("CONVERT TO CSR: %fms\n", TIMER_ELAPSED(0) / 1.e3);
 
-    // TIMING OF GEMM_SPARSE --------------------------------------------------------
+    // TIMING OF spmv_SPARSE --------------------------------------------------------
     int wrong = 0;
     for (int cycle = -WARM_CYCLES; cycle < TEST_CYCLES; cycle++) {
         // Reset
-        bzero(res1, ROWS * sizeof(double));
-        bzero(res2, ROWS * sizeof(double));
+        bzero(res1, ROWS * sizeof(M_TYPE));
+        bzero(res2, ROWS * sizeof(M_TYPE));
 
         // TEST
-        TIMER_TIME(0, gemm_sparse_coo(x, y, vals, vec, res1, NON_ZERO));
+        TIMER_TIME(0, spmv_coo(x, y, vals, vec, res1, NON_ZERO));
         if (cycle >= 0)
             times_coo[cycle] = TIMER_ELAPSED(0) / 1.e6;
 
-        TIMER_TIME(1, gemm_sparse_csr(x, csr_y, vals, vec, res2, ROWS));
+        TIMER_TIME(1, spmv_csr(x, csr_y, vals, vec, res2, ROWS));
         if (cycle >= 0)
             times_csr[cycle] = TIMER_ELAPSED(1) / 1.e6;
 
         for (int i = 0; i < ROWS; i++) {
-            if ((res1[i] - res2[i]) / res1[i] < 0.01)
+            if ((res1[i] - res2[i]) / res1[i] > 0.01) {
                 wrong++;
+            }
+        }
+
+        TIMER_TIME(1, optimized_spmv_csr(x, csr_y, vals, vec, res2, ROWS, NON_ZERO));
+        if (cycle >= 0)
+            times_opt_csr[cycle] = TIMER_ELAPSED(1) / 1.e6;
+
+        for (int i = 0; i < ROWS; i++) {
+            if ((res1[i] - res2[i]) / res1[i] > 0.01) {
+                wrong++;
+            }
         }
     }
 
     // TIMING ------------------------------------------------------------------
     print_time_data("COO", times_coo, TEST_CYCLES, 2 * NON_ZERO);
     print_time_data("CSR", times_csr, TEST_CYCLES, 2 * NON_ZERO);
-    printf("WRONG %d", wrong);
-
+    print_time_data("OPT CSR", times_opt_csr, TEST_CYCLES, 2 * NON_ZERO);
+    printf("WRONG %d\n", wrong);
 // FREE  -------------------------------------------------------------------
 free:
     free(x);
@@ -162,6 +191,7 @@ free:
     free(res2);
     free(times_coo);
     free(times_csr);
+    free(times_opt_csr);
     fclose(file);
     return ret;
 }
