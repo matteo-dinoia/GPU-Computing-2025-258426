1db2f6f344b379f4c1e2e82b9208eefb61393e62
Matteo Di Noia <dmatteo002@gmail.com>
2025-06-25 12:44:01 +0200

    Add extra print to main

--- deliverable2/src/main.cu
+++ deliverable2/src/main.cu
@@ -35,11 +35,13 @@ int main(const int argc, char** argv)
 
 int timed_main(const char* input_file)
 {
+    cout << "\n* Started" << endl;
     TIMER_DEF(0);
     TIMER_DEF(1);
     TIMER_DEF(2);
     TIMER_DEF(3);
     TIMER_DEF(4);
+    TIMER_DEF(5);
 
     // Data allocation
     GpuCoo<uint32_t, float> matrix = {0, 0, 0, nullptr, nullptr, nullptr};
@@ -56,6 +58,7 @@ int timed_main(const char* input_file)
         return -1;
     }
     TIMER_STOP(0);
+    cout << "* Read data" << endl;
 
     // Alloc memory
     TIMER_START(1);
@@ -66,42 +69,50 @@ int timed_main(const char* input_file)
     cudaMallocManaged(&matrix.ys, matrix.NON_ZERO * sizeof(int));
     cudaMallocManaged(&matrix.vals, matrix.NON_ZERO * sizeof(float));
 
-    // Copy data to GPU
-    cudaMemcpy(matrix.xs, coo->col, matrix.NON_ZERO * sizeof(uint32_t), cudaMemcpyHostToDevice);
-    cudaMemcpy(matrix.ys, coo->row, matrix.NON_ZERO * sizeof(uint32_t), cudaMemcpyHostToDevice);
-    cudaMemcpy(matrix.vals, coo->val, matrix.NON_ZERO * sizeof(float), cudaMemcpyHostToDevice);
-
     // Alloc memory for other part
     cudaMallocManaged(&vec, matrix.COLS * sizeof(float));
     cudaMallocManaged(&res, matrix.ROWS * sizeof(float));
     cudaMallocManaged(&res_control, matrix.ROWS * sizeof(float));
     TIMER_STOP(1);
+    cout << "* Allocated  memory" << endl;
+
+    // Copy data to GPU
+    TIMER_START(2);
+    cudaMemcpy(matrix.xs, coo->col, matrix.NON_ZERO * sizeof(uint32_t), cudaMemcpyHostToDevice);
+    cudaMemcpy(matrix.ys, coo->row, matrix.NON_ZERO * sizeof(uint32_t), cudaMemcpyHostToDevice);
+    cudaMemcpy(matrix.vals, coo->val, matrix.NON_ZERO * sizeof(float), cudaMemcpyHostToDevice);
+    TIMER_STOP(2);
+    cout << "* Copied COO to GPU memory" << endl;
 
     // Generation of random vector
-    TIMER_START(2);
+    TIMER_START(3);
     randomize_dense_vec(vec, matrix.ROWS);
-    TIMER_STOP(2);
+    TIMER_STOP(3);
+    cout << "* Randomized Vector" << endl;
 
     // Execution
-    TIMER_START(3);
+    TIMER_START(4);
     execution(matrix, vec, res, res_control);
-    TIMER_STOP(3);
+    TIMER_STOP(4);
+    cout << "* Terminated execution" << endl;
 
     // Free memory and close resources
-    TIMER_START(4);
+    TIMER_START(5);
     cudaFree(matrix.xs);
     cudaFree(matrix.ys);
     cudaFree(matrix.vals);
     cudaFree(vec);
     cudaFree(res_control);
     cudaFree(res);
-    TIMER_STOP(4);
+    TIMER_STOP(5);
+    cout << "* Finished Deallocating\n" << endl;
 
     // Print time
     cout << "Time elapsed for reading: " << TIMER_ELAPSED_MS(0) << " ms" << endl;
-    cout << "Time elapsed for allocation and copy: " << TIMER_ELAPSED_MS(1) << " ms" << endl;
-    cout << "Time elapsed for vector generation: " << TIMER_ELAPSED_MS(2) << " ms" << endl;
-    cout << "Time elapsed for full tester: " << TIMER_ELAPSED_MS(3) << " ms" << endl;
-    cout << "Time elapsed for free : " << TIMER_ELAPSED_MS(4) << " ms" << endl;
+    cout << "Time elapsed for allocation: " << TIMER_ELAPSED_MS(1) << " ms" << endl;
+    cout << "Time elapsed for copy: " << TIMER_ELAPSED_MS(2) << " ms" << endl;
+    cout << "Time elapsed for vector generation: " << TIMER_ELAPSED_MS(3) << " ms" << endl;
+    cout << "Time elapsed for full tester: " << TIMER_ELAPSED_MS(4) << " ms" << endl;
+    cout << "Time elapsed for free : " << TIMER_ELAPSED_MS(5) << " ms" << endl;
     return 0;
 }
