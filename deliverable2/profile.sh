fe6c093f6612843cb6bb074610c3eff54c67a112
Matteo Di Noia <dmatteo002@gmail.com>
2025-07-05 16:45:19 +0200

    Fix profiler

--- deliverable2/profile.sh
+++ deliverable2/profile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'COMPILING'
-make normal || exit;
+make profiling || exit;
 
 echo 'PROFILING (may take some time to finish)';
-sudo $(which ncu) --config-file off --export report.ncu-rep --force-overwrite --set full build/main $1
+sudo $(which ncu) --config-file off --export report.ncu-rep --force-overwrite --set full build/main_profiling $1
